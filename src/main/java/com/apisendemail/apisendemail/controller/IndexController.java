f267e44c41ba136a0a42997a4a0c05a091597eec
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2025-08-03 11:42:03 -0300

    Usando CrossOrigin para permitir todas os endereços usarem o endpoint

--- src/main/java/com/apisendemail/apisendemail/controller/IndexController.java
+++ src/main/java/com/apisendemail/apisendemail/controller/IndexController.java
@@ -3,13 +3,11 @@ package com.apisendemail.apisendemail.controller;
 import com.apisendemail.apisendemail.dto.RequestMessage;
 import com.apisendemail.apisendemail.service.SendMessageService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "*")
 public class IndexController {
 
     private final SendMessageService sendMessageService;
