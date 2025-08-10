dcde8516338d42f950c88b9ae722dbb0205623c4
lucas <lucasolisocialmedia@gmail.com>
2025-08-10 20:32:02 -0300

    usando Classes do Mime para poder retornar a mensagem com corpo HTML

--- src/main/java/com/apisendemail/apisendemail/service/SendMessageService.java
+++ src/main/java/com/apisendemail/apisendemail/service/SendMessageService.java
@@ -2,10 +2,13 @@ package com.apisendemail.apisendemail.service;
 
 import com.apisendemail.apisendemail.dto.RequestMessage;
 import com.apisendemail.apisendemail.model.Mensagem;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,19 +20,20 @@ public class SendMessageService {
     @Value("${spring.mail.username}")
     public String toEmail;
 
-    public void sendMessage(RequestMessage requestMessage) {
+    public void sendMessage(RequestMessage requestMessage) throws MessagingException {
         String htmlContent = Mensagem.montarMensagem(
                 requestMessage.fromEmail(),
                 requestMessage.body()
         );
 
-        SimpleMailMessage message = new SimpleMailMessage();
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper message = new MimeMessageHelper(mimeMessage, true, "UTF-8");
 
         message.setFrom(requestMessage.fromEmail());
         message.setTo(toEmail);
         message.setSubject(requestMessage.subject());
-        message.setText(htmlContent);
+        message.setText(htmlContent, true);
 
-        mailSender.send(message);
+        mailSender.send(mimeMessage);
     }
 }
